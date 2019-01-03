ded70f011544f7f79804cb9d1ee7a93e1477cf45
homoluctus <w.slife18sy@gmail.com>
2019-01-03 12:50:56 +0900

    Add FlagHandler to convert flag bit to human readable

--- tcp.py
+++ tcp.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from utils import bin2str, bin2int
 
 class TCP:
@@ -21,7 +22,7 @@ class TCP:
         return (self.packet[12] >> 4) * 4
 
     def control_flag(self):
-        return hex(self.packet[13])
+        return FlagHandler(self.packet[13])
 
     def window_size(self):
         return bin2int(self.packet[14:16])
@@ -31,3 +32,31 @@ class TCP:
 
     def urgent_pointer(self):
         return bin2str(self.packet[18:20])
+
+class FlagHandler:
+    """
+    Return a list includes 2-tuple (Flag.name, hex(Flag.value))
+    """
+
+    def __new__(cls, value):
+        if not isinstance(value, int):
+            raise TypeError("%r can't be interpreted as an integer" % value)
+
+        return cls._get(value)
+
+    @classmethod
+    def _get(cls, value):
+        return [(member.name, hex(member.value)) for key, member in Flag._value2member_map_.items() if (key & value) != 0x0]
+
+class Flag(Enum):
+    CWR = 0x80
+    ECE = 0x40
+    URG = 0x20
+    ACK = 0x10
+    PSH = 0x08
+    RST = 0x04
+    SYN = 0x02
+    FIN = 0x01
+
+    def __str__(self):
+        return self.name
