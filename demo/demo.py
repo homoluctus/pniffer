b65140c3ea03f0990bea2fcb968dc9a084c35d71
homoluctus <w.slife18sy@gmail.com>
2019-01-08 05:45:50 +0900

    Remove promiscuous mode procedure

--- demo/demo.py
+++ demo/demo.py
@@ -1,11 +1,13 @@
 import struct
 import socket
 
+
 try:
     from pniffer.ethernet import Ethernet
     from pniffer.ipv4 import IPv4
     from pniffer.tcp import TCP
     from pniffer.udp import UDP
+    from promiscuous import promiscuous_mode
 except (ModuleNotFoundError, ImportError):
     import sys
     from traceback import print_exc
@@ -59,30 +61,11 @@ if __name__ == '__main__':
     # For proto argument of socket object
     ETH_P_ALL = 3   # All protocol pakcet
 
-    # For socket.setsockopt()
-    SOL_PACKET = 263    # Socket level
-    PACKET_ADD_MEMBERSHIP = 1   # Socket option name
-
-    # For mr_type (action) of packet_mreq structure
-    PACKET_MR_PROMISC = 1   # Promiscuous mode
-
-    # For packet_mreq structure
-    mr_ifindex = socket.if_nametoindex(interface)   # c_type is int
-    mr_type = PACKET_MR_PROMISC                     # c_type is unsigned short
-    mr_alen = 0           # c_type is unsigned short
-    mr_address = b'\0'    # c_type is unsigned char[8]
-
-    packet_mreq = struct.pack('iHH8s',
-                              mr_ifindex,
-                              mr_type,
-                              mr_alen,
-                              mr_address)
-
     with socket.socket(socket.AF_PACKET,
                        socket.SOCK_RAW,
                        socket.htons(ETH_P_ALL)) as sock:
         sock.bind((interface, 0))
-        sock.setsockopt(SOL_PACKET, PACKET_ADD_MEMBERSHIP, packet_mreq)
+        promiscuous_mode(sock, interface)        
 
         try:
             while True:
